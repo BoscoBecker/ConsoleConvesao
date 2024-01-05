6bdc5f320e7cd444501a2b05dee83ed535893202
DESKTOP-G0TT3JA\Usuário <joao.jbosco@gmail.com>
2024-01-05 19:57:43 -0300

    Confirmando alteração

--- ConsoleConvesao/Program.cs
+++ ConsoleConvesao/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bom dia");
+﻿using System.Globalization;
+// See https://aka.ms/new-console-template for more information
 
+char genero = 'F';
+int idade = 32;
+double saldo = 10.35784;
+string nome = "Sofia";
+
+Console.WriteLine("Bom dia");
+Console.WriteLine("Boa Tarde");
+Console.WriteLine("Boa noite");
+Console.WriteLine("--------------------------");
+Console.WriteLine(genero);
+Console.WriteLine(idade);
+Console.WriteLine(saldo.ToString("F2"));
+Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
+// Concatenação
+Console.WriteLine(nome + " tem a idade de " + idade + " anos e tem o Saldo de " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+// Usando placeHolders
+Console.WriteLine("{0} tem a idade de {1} anos e tem o saldo de {2:F2} reais.", nome, idade, saldo.ToString("F2", CultureInfo.InvariantCulture));
+// Interpolação + 6
+Console.WriteLine($"{nome} tem a idade de " + $"{idade}" + " anos e tem o saldo de " + $"{saldo.ToString("F2", CultureInfo.InvariantCulture)} reais.");
